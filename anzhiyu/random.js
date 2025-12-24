f8f044649a5c06d205b115ea435a6d61d046ba26
yxcgsc <lgwnf2123233252@gmail.com>
2025-12-25 01:28:05 +0800

    Add files via upload

---
+++ anzhiyu/random.js
@@ -0,0 +1,3 @@
+var posts=["/cg/Day Dream/","/cg/Colony City 27 Lambda/","/cg/傲慢的怪兽公主与名侦探使魔/","/cg/Miss Neko-Pirates/","/cg/Rebirth Ii/","/cg/ReturnAtis/","/cg/mashGirls/","/cg/Yahtzee Girl/","/cg/kamihikoki_mmd/","/cg/yxcg/","/cg/不双修就去世/","/cg/Half-Demon/","/cg/SummerForYou/","/cg/地下偶像/","/cg/AURA:Cards/","/cg/妖语·Yokai'sSecret/","/cg/Seek Girl:Fog Ⅰ/","/cg/TakeMeToThe/","/cg/ksyl/","/cg/Resist/","/cg/暴食的怪兽公主/","/cg/ドゥアズ～神父様ノ言いなり/","/cg/Dead-End/","/cg/Love/","/cg/Miss Neko/","/cg/ProjectIsland/","/cg/2LOST2/","/cg/Sweet/","/cg/MMYTD/","/cg/Mirai/"];function toRandomPost(){
+    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
+  };
